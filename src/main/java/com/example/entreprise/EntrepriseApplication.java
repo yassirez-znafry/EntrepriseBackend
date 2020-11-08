531d27cbb36a9040b5571f3108895be847c4f7da
yassirez-znafry <znafryassir@gmail.com>
2020-11-08 17:50:11 +0100

    second commit: i added the controllers, the models, the services, and the repositories that handle the "candidature".

--- src/main/java/com/example/entreprise/EntrepriseApplication.java
+++ src/main/java/com/example/entreprise/EntrepriseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EntrepriseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EntrepriseApplication.class, args);
 	}
 
