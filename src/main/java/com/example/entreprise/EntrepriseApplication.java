2ebb243ff2e96216e902896b6a219696758075d6
yassirez-znafry <znafryassir@gmail.com>
2020-11-08 17:52:06 +0100

    In this commit, i change the banner of my application from "Spring" to "Entreprise"

--- src/main/java/com/example/entreprise/EntrepriseApplication.java
+++ src/main/java/com/example/entreprise/EntrepriseApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EntrepriseApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(EntrepriseApplication.class, args);
 	}
 
